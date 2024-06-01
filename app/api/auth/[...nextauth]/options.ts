97e9ca819b991d3fd75dca7751c45cd87b616383
Danylo Dziubchuk <vt211_ddr@student.ztu.edu.ua>
2024-06-01 23:29:54 +0300

    changed some staff

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -1,6 +1,38 @@
 import type { NextAuthOptions } from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google";
+import { createPool } from '@vercel/postgres';
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+async function addUserIfNotExists(email: string, name: string) {
+  const connectionString = process.env.POSTGRES_URL as string;
+  const client = createPool({ connectionString });
+
+  try {
+    const user = await client.sql`
+      SELECT * FROM users WHERE email = ${email}
+    `;
+
+    // @ts-ignore
+    if (user.length === 0) {
+      await client.sql`
+        INSERT INTO users (name, email)
+        VALUES (${name}, ${email})
+        ON CONFLICT (email) DO NOTHING;
+      `;
+      console.log(`Added new user: ${name}`);
+    }
+  } catch (error) {
+    console.error('Error adding user:', error);
+    throw error;
+  } finally {
+    await client.end();
+  }
+}
+
 
 
 export const options : NextAuthOptions = {
@@ -14,10 +46,14 @@ export const options : NextAuthOptions = {
     clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
   })
   ],
+  
   secret: process.env.NEXTAUTH_SECRET as string,
   callbacks:{
-    async signIn() {
-      return true
+    async signIn({ user, account, profile }) {
+      const email = user.email as string;
+      const name = user.name as string;
+      await addUserIfNotExists(email, name);
+      return true;
     },
   },
 }
